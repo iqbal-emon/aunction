d4acafde329831c241abdf5d5d473177f209d70d
Mohammad Iqbal <emon@ndessl.local>
2023-12-17 19:26:36 +0600

    last day

---
+++ Controllers/HomeController5.cs
@@ -0,0 +1,76 @@
+﻿using aunction.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Threading.Tasks;
+
+public class HomeController5 : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+
+    public HomeController5(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    [HttpPost]
+    [Route("Home/UpdateProduct/{ItemID}")]
+    public async Task<IActionResult> UpdateProduct(int ItemID, [FromForm] addProudct product)
+    {
+        try
+        {
+            using (SqlConnection con = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                await con.OpenAsync();
+
+                string updateQuery = "UPDATE items SET Title = @Title, Description = @Description, " +
+                                     "Category = @Category, ReservePrice = @ReservePrice, " +
+                                     "ImageURL = COALESCE(CAST(@ImageURL AS VARBINARY(MAX)), ImageURL), " +
+                                     "StartTime = @StartTime, EndTime = @EndTime " +
+                                     "WHERE itemID = @ItemID";
+
+                using (SqlCommand cmd = new SqlCommand(updateQuery, con))
+                {
+                    cmd.Parameters.AddWithValue("@ItemID", product.ItemID);
+                    cmd.Parameters.AddWithValue("@Title", product.Title);
+                    cmd.Parameters.AddWithValue("@Description", product.Description);
+                    cmd.Parameters.AddWithValue("@Category", product.Category);
+                    cmd.Parameters.AddWithValue("@ReservePrice", product.ReservePrice);
+                    cmd.Parameters.AddWithValue("@StartTime", product.StartTime);
+                    cmd.Parameters.AddWithValue("@EndTime", product.EndTime);
+
+                    if (product.ImageURL != null && product.ImageURL.Length > 0)
+                    {
+                        using (var memoryStream = new MemoryStream())
+                        {
+                            await product.ImageURL.CopyToAsync(memoryStream);
+                            byte[] imageBytes = memoryStream.ToArray();
+                            cmd.Parameters.AddWithValue("@ImageURL", imageBytes);
+                        }
+                    }
+                    else
+                    {
+                        cmd.Parameters.AddWithValue("@ImageURL", DBNull.Value);
+                    }
+
+                    int rowsAffected = await cmd.ExecuteNonQueryAsync();
+                    Console.WriteLine($"Rows Affected: {rowsAffected}");
+                }
+
+                await con.CloseAsync();
+            }
+
+            return Ok("Product updated successfully");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+            return StatusCode(StatusCodes.Status500InternalServerError, $"Error: {ex.Message}");
+        }
+    }
+}
+
---
+++ Controllers/HomeController7.cs
@@ -0,0 +1,58 @@
+﻿using aunction.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+[ApiController]
+public class HomeController7 : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+
+    public HomeController7(IConfiguration config)
+    {
+        _configuration = config;
+    }
+
+    [HttpGet]
+    [Route("Home/Win/{CustomerID}")]
+    public List<bidStatus> GetAllDetails(string CustomerID)
+    {
+        List<bidStatus> Lst = new List<bidStatus>();
+
+        using (SqlConnection con = new SqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+        {
+            con.Open();
+
+            using (SqlCommand cmd = new SqlCommand("SELECT TOP 1 * FROM bids " +
+                                       "INNER JOIN Customers ON Customers.CustomerID = Bids.CustomerID " +
+                                       "WHERE bids.CustomerID = @CustomerID " +
+                                       "ORDER BY Bids.Amount DESC", con))
+            {
+                cmd.Parameters.AddWithValue("@CustomerID", CustomerID);
+
+                using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                {
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        bidStatus dto = new bidStatus
+                        {
+                            ItemID = Convert.ToInt32(row["ItemID"]),
+                            CustomerID = Convert.ToString(row["CustomerID"]),
+                            Amount = Convert.ToString(row["Amount"])
+                        };
+
+                        Lst.Add(dto);
+                    }
+                }
+            }
+        }
+
+        return Lst;
+    }
+}
